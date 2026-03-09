45c61bf2bba6c5619cfcc6ced64d2b06f9aff4e4
MarcioJuniorDev <marciojunior2911@gmail.com>
2026-03-09 16:39:53 -0300

    inrodução a navegação

--- aula02/src/main.jsx
+++ aula02/src/main.jsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import routes from '~pages';
+
+// cria o roteador
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
